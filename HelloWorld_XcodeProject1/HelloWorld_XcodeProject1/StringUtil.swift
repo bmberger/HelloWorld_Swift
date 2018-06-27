50dff85afebfb5af3b26726da83d979eeb1515e2
Briana Berger <brianamberger@gmail.com>
2018-06-27 16:14:57 -0700

    Add files via upload

---
+++ HelloWorld_XcodeProject1/HelloWorld_XcodeProject1/StringUtil.swift
@@ -0,0 +1,16 @@
+//
+//  ReturnableViewController.swift
+//  HelloWorld_XcodeProject1
+//
+//  Created by Briana Berger on 6/27/18.
+//  Copyright © 2018 Briana Berger. All rights reserved.
+//
+
+struct StringUtil {
+    
+    func printHelloName(input : String) -> String
+    {
+        return "Hello, " + input + ". Welcome to Expedia!"
+    }
+    
+}
